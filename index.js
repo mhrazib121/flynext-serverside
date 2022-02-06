8206997916b7c6603e5c9d32aeae268c0547c45f
mhrazib121 <mhrazib121@gmail.com>
2022-02-06 19:20:06 +0600

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -10,7 +10,7 @@ app.use(express.json());
 // qGiMK8dbEDmBzgM1
 
 
-const uri = "mongodb+srv://flynext:qGiMK8dbEDmBzgM1@cluster0.xvulc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvulc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // client.connect(err => {
 //   const collection = client.db("test").collection("devices");
@@ -30,8 +30,20 @@ async function run(){
         await client.connect();
         console.log('connected');
         const database = client.db("flynext");
+        const allServiceCollection = database.collection("allService");
         const allBookingCollection = database.collection("allBooking");
 
+        app.get('/services', async(req, res)=>{
+            const cursor = allServiceCollection.find({});
+            const service = await cursor.toArray();
+            res.send(service);
+        })
+
+        app.post('/services', async(req, res)=>{
+            const service = req.body;
+            const result = await allServiceCollection.insertOne(service);
+            res.send(result);
+        })
         
         // Booking get 
         app.get('/bookings', async(req, res) =>{
@@ -46,8 +58,26 @@ async function run(){
             res.json(result);
         })
 
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await allBookingCollection.deleteOne(query);
+            res.json(result);
+        })
 
-
+        app.put('/bookings/:id', async(req, res)=>{
+            const id = req.params.id;
+            const updateBookingStatus = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc ={
+                $set:{
+                    status: updateBookingStatus.status
+                }
+            };
+            const result = await allBookingCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        })
     }
     finally{
 
