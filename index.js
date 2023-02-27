38d1e233c53593bfdc2e913c5aee932ddbdc7714
razib121 <mhrazib121@gmail.com>
2023-02-27 17:00:39 +0600

    fetching error solved

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
+//Middleware
 app.use(cors());
 app.use(express.json());
+
+const corsConfig = {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,authorization"
+    );
+    next();
+});
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvulc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
